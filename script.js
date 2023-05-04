e95fa9c45e6e0cfc4a7e1b162d1f76660447c1eb
aheeb <78415840+aheeb@users.noreply.github.com>
2023-05-04 10:19:11 +0200

    V1 mit fade-in and out

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 function getNewQuote() {
-    fetch('data.php')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById("quote").innerHTML = data.quote;
-            document.getElementById("author").innerHTML = "- " + data.author;
-        })
-        .catch(error => console.error(error));
-}
-
-document.addEventListener("DOMContentLoaded", getNewQuote);
-setInterval(getNewQuote, 5000);
+    fetch("data.php")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const quoteElement = document.getElementById("quote");
+        const authorElement = document.getElementById("author");
+  
+        // Fade-out-Animation starten
+        quoteElement.classList.add("fade-out");
+        authorElement.classList.add("fade-out");
+  
+        // Nachdem die Fade-out-Animation abgeschlossen ist
+        setTimeout(() => {
+          // Zitat und Autor aktualisieren
+          quoteElement.innerHTML = data.quote;
+          authorElement.innerHTML = "- " + data.author;
+  
+          // Fade-in-Animation starten
+          quoteElement.classList.remove("fade-out");
+          authorElement.classList.remove("fade-out");
+          quoteElement.classList.add("fade-in");
+          authorElement.classList.add("fade-in");
+  
+          // Fade-in-Animation entfernen, damit sie beim nächsten Mal wieder angewendet werden kann
+          setTimeout(() => {
+            quoteElement.classList.remove("fade-in");
+            authorElement.classList.remove("fade-in");
+          }, 2000);
+        }, 2000);
+      })
+      .catch((error) => console.error(error));
+  }
+  
+  document.addEventListener("DOMContentLoaded", getNewQuote);
+  setInterval(getNewQuote, 7000);
+  
