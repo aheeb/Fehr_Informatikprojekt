fbb7c8713d81ae079b98c50485f350f3c8466f62
aheeb <78415840+aheeb@users.noreply.github.com>
2023-06-07 11:21:48 +0200

    mit stats bereinigen

---
+++ clearstats.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "quotout";
+$password = "qu0t_";
+$dbname = "citation";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE citation SET views = 0";
+if (mysqli_query($conn, $sql)) {
+    // Hier keine Ausgabe machen, da dies die Verwendung von header() unten stört
+} else {
+    echo "Fehler beim Zurücksetzen der Views: " . mysqli_error($conn);
+    exit();  // Beenden Sie das Skript, wenn ein Fehler auftritt, um die Weiterleitung unten zu verhindern
+}
+
+$conn->close();
+
+// Weiterleitung zur stats.html-Seite
+header("Location: stats.html");
+exit();  // Beenden Sie das Skript nach der Weiterleitung, um sicherzustellen, dass kein weiterer Code ausgeführt wird
+?>
